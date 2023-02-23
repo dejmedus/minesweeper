437aa3188e007fd4f54947b984e544ecae350945
dejmedus <email96123@gmail.com>
2023-02-23 09:55:49 -0700

    add dynamic score numbers

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
+console.log("working");
+
 // sweeper buttons
-const game = document.getElementId("game");
+const game = document.getElementById("game");
 for (let i = 0; i < 90; i++) {
     const btn = document.createElement("button");
     if (i < 10) {
@@ -14,34 +16,49 @@ for (let i = 0; i < 90; i++) {
 }
 
 // game status
-const statusEl = document.getElementId("status");
+const statusEl = document.getElementById("status");
 
-let gameStatus = playing;
+let gameStatus = "playing";
 
-statusEl.innerHTML = `<img src="./assets/status/${gameStatus}.png" alt="game status smiley face">`
+statusEl.innerHTML = `<img src="./assets/status/${gameStatus}.svg" alt="game status smiley face">`
 
-const scoreEl = document.getElementId("score");
-const triesEl = document.getElementId("tries");
+const scoreEl = document.getElementById("score");
+const triesEl = document.getElementById("tries");
 
 // status numbers
 let score = 12;
-let tries = 04;
+let tries = 4;
 
-statusNumbers(score);
-statusNumbers(tries);
+statusNumbers(scoreEl, score);
+statusNumbers(triesEl, tries);
 
 function statusNumbers(element, num) {
+    const numStr = num.toString();
     const zero = `<img src="./assets/status-numbers/0.png" alt="number 0">`;
 
-    element.innerHTML += zero;
-
-    if (num < 10) {
+    if (num == 0) {
+        element.innerHTML += zero;
+        element.innerHTML += zero;
         element.innerHTML += zero;
     }
-    else {
-        element.innerHTML += `<img src="./assets/status-numbers/${num[0]}.png" alt="number ${num[0]}">`
+    else if (num >= 100) {
+        element.innerHTML += `<img src="./assets/status-numbers/${numStr[0]}.png" alt="number ${numStr[0]}">`
+        element.innerHTML += `<img src="./assets/status-numbers/${numStr[1]}.png" alt="number ${numStr[1]}">`
+        element.innerHTML += `<img src="./assets/status-numbers/${numStr[2]}.png" alt="number ${numStr[2]}">`
     }
-    element.innerHTML += `<img src="./assets/status-numbers/${num[1]}.png" alt="number ${num[1]}">`
+    else {
+        element.innerHTML += zero;
+
+        if (num < 10) {
+            element.innerHTML += zero;
+            element.innerHTML += `<img src="./assets/status-numbers/${numStr[0]}.png" alt="number ${numStr[0]}">`
+        }
+        else {
+            element.innerHTML += `<img src="./assets/status-numbers/${numStr[0]}.png" alt="number ${numStr[0]}">`
+            element.innerHTML += `<img src="./assets/status-numbers/${numStr[1]}.png" alt="number ${numStr[1]}">`
+        }
+    }
+
 }
 
 
