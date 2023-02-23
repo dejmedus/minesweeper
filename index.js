9a23c549a6d0545b3ef170db1b3bb67222ce7c19
dejmedus <email96123@gmail.com>
2023-02-23 12:49:18 -0700

    create minemap, end game when a mine is clicked on

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-// global vars
 const difficultySettings = {
-    beginner: 10,
-    intermediate: 8,
-    expert: 5
+    beginner: [10, 5],
+    intermediate: [8, 8],
+    expert: [5, 10]
 }
 
 let seconds = 0;
 let gameStarted = false;
+let gameOver = false;
 
 const statusEl = document.getElementById("status");
 const secondsEl = document.getElementById("seconds");
@@ -19,7 +19,9 @@ setupGame();
 function setupGame() {
     // set initial values
     seconds = 0;
-    let flags = difficultySettings.beginner;
+    gameOver = false;
+    let flags = difficultySettings.beginner[0];
+    let mineMap = [];
 
     // set initial flags to zero
     setStatusNumbers(flagsEl, flags);
@@ -35,11 +37,23 @@ function setupGame() {
     })
     setStatus("playing", 'smiley face');
 
+    // generate mine map
+    for (let i = 0; i < difficultySettings.beginner[1]; i++) {
+        let index = Math.floor(Math.random() * 90 + 1);
+        while (mineMap.includes(index)) {
+            index = Math.floor(Math.random() * 90 + 1);
+        }
+        mineMap.push(index);
+    }
 
     // populate sweeper tiles
     game.replaceChildren();
     for (let i = 0; i < 90; i++) {
         const tile = document.createElement("button");
+        if (mineMap.includes(i)) {
+            tile.classList.add("mine");
+        }
+
         if (i < 10) {
             tile.id = `0${i}`
         }
@@ -55,22 +69,41 @@ function setupGame() {
 
     for (let i = 0; i < tiles.length; i++) {
         tiles[i].addEventListener('click', (e) => {
-            if (gameStarted == false) {
-                startGame();
+            if (!gameOver) {
+                !gameStarted && startGame();
+
+                e.target.classList.add("found");
+
+                // if tile has a mine
+                if (e.target.classList.contains('mine')) {
+                    e.target.innerHTML = `<img src="./assets/icons/mine.svg" alt="mine">`;
+
+                    gameStarted = false;
+                    gameOver = true;
+
+                    tiles.forEach(tile => {
+                        tile.setAttribute('disabled', true);
+                    })
+
+                    setStatus("lost", "upset smiley face");
+                }
             }
-            e.target.classList.add("found");
         });
 
         tiles[i].addEventListener('contextmenu', (e) => {
-            // prevent context menu from opening
-            e.preventDefault();
+            if (!gameOver) {
+                !gameStarted && startGame();
 
-            if (flags > 0) {
-                e.target.classList.add("flag");
-                e.target.innerHTML = `<img src="./assets/icons/flag.svg" alt="flag">`
+                // prevent context menu from opening
+                e.preventDefault();
 
-                flags--;
-                setStatusNumbers(flagsEl, flags);
+                if (flags > 0) {
+                    e.target.classList.add("flag");
+                    e.target.innerHTML = `<img src="./assets/icons/flag.svg" alt="flag">`
+
+                    flags--;
+                    setStatusNumbers(flagsEl, flags);
+                }
             }
         });
     }
