d06d7907f919bd590c6f1091cc20c2c13c7bc0fb
dejmedus <email96123@gmail.com>
2023-02-26 11:30:34 -0700

    fix click to bring to front by targetting dataset

--- helpers/drag.js
+++ helpers/drag.js
@@ -1,52 +1,64 @@
 // https://stackoverflow.com/questions/9334084/moveable-draggable-div
 const titleBars = [...document.querySelectorAll('.title-bar')];
-let parentElements = [];
+let titleBarParents = [];
+const parentElements = [...document.querySelectorAll('[data-clicks]')];
+
 let clickOrder = [];
 
 let element;
 
 let x = 0;
 let y = 0;
-for (let i = 0; i < titleBars.length; i++) {
-    titleBars[i].addEventListener('mousedown', (e) => {
 
-        element = parentElements[i];
+for (let i = 0; i < titleBars.length; i++) {
+    // get parent elements to drag
+    element = titleBars[i];
+    while (!element.hasAttribute('id')) {
+        element = element.parentNode;
+    }
+    titleBarParents.push(element);
+
+    // dragging brings to front
+    titleBars[i].addEventListener('mousedown', (e) => {
+        element = titleBarParents[i];
+
+        e.stopPropagation()
+        let index = parentElements.findIndex(element => element.id == e.target.dataset.clicks);
 
         setZIndex({
-            element: parentElements[i],
-            name: `${parentElements[i]}`
+            element: parentElements[index],
+            name: e.target.dataset.clicks
         });
 
         x = e.offsetX;
         y = e.offsetY;
         window.addEventListener('mousemove', drag);
     }, true);
+}
 
-    // get parent elements to drag
-    element = titleBars[i];
-    while (!element.hasAttribute('id')) {
-        element = element.parentNode;
-    }
-    parentElements.push(element);
+// clicking anywhere on a div brings it to front
+for (let i = 0; i < parentElements.length; i++) {
+    parentElements[i].addEventListener('click', (e) => {
+        e.stopPropagation()
+        let index = parentElements.findIndex(element => element.id == e.target.dataset.clicks);
 
-    // clicking anywhere on a div brings it to front
-    element.addEventListener('click', (e) => {
         setZIndex({
-            element: parentElements[i],
-            name: `${parentElements[i]}`
+            element: parentElements[index],
+            name: e.target.dataset.clicks
         });
     })
 }
 
+
+
 function setZIndex(div) {
-    if (clickOrder.includes(`${div.name}`)) {
-        clickOrder.splice(clickOrder.findIndex(div.name), 1);
-    }
-    clickOrder.push(div.element);
+    clickOrder.filter(element => element.name !== div.name);
+    clickOrder.push({ element: div.element, name: div.name });
 
     for (let i = 0; i < clickOrder.length; i++) {
-        clickOrder[i].style.zIndex = i + 1;
+        clickOrder[i].element.style.zIndex = i + 1;
     }
+    console.log(clickOrder);
 }
 
 function drag(e) {
